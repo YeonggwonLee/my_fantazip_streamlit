3b0724fde22eda52e8454041c284397be0ce45bc
YeonggwonLee <lyg041774@gmail.com>
2024-03-19 11:05:44 +0900

    version6

--- pages/4_🌏동네 시설 지도 서비스.py
+++ pages/4_🌏동네 시설 지도 서비스.py
@@ -110,9 +110,9 @@ def plot_trends(df_combined, selected_dong, building_type_list):
                 plt.text(x, y, f'{y:.0f}', color=color_palette[i], ha='center', va='bottom',fontproperties=prop)
 
         average_data = filtered_data[filtered_data['건물용도'] == building_type].groupby('접수년도')['평당평균보증금(만원)'].mean().reset_index()
-        sns.lineplot(data=average_data, x='접수년도', y='평당평균보증금(만원)', label=f'서울시 평균 - {building_type}', linestyle='--', linewidth=2, color=color_palette[i],fontproperties=prop)
+        sns.lineplot(data=average_data, x='접수년도', y='평당평균보증금(만원)', label=f'서울시 평균 - {building_type}', linestyle='--', linewidth=2, color=color_palette[i])
 
-    plt.title(f'{selected_dong} 연도별 평당평균보증금 변화',fontproperties=propgit)
+    plt.title(f'{selected_dong} 연도별 평당평균보증금 변화',fontproperties=prop)
     plt.xlabel('접수년도',fontproperties=prop)
     plt.ylabel('평당평균보증금(만원)',fontproperties=prop)
     plt.legend()
